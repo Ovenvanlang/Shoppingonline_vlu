39f175d2c28c2dd1e419b86310b86e67b2d5ca34
Khanxay <stormsuper21@gmail.com>
2026-04-03 17:41:06 +0700

    Add files via upload

---
+++ client-admin/src/components/MenuComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import MyContext from "../contexts/MyContext";
+import { Link } from "react-router-dom";
+
+class Menu extends Component {
+  static contextType = MyContext;
+
+  render() {
+    return (
+      <div className="border-bottom">
+        <div className="float-left">
+          <ul className="menu">
+            <li className="menu">
+              <Link to="/admin/home">Home</Link>
+            </li>
+
+            {/* ✅ FIX: bỏ li lồng li */}
+            <li className="menu">
+              <Link to="/admin/category">Category</Link>
+            </li>
+
+            <li className="menu">
+              <Link to="/admin/product">Product</Link>
+            </li>
+
+            <li className="menu">
+              <Link to="/admin/order">Order</Link>
+            </li>
+
+            {/* ✅ UPDATE: thêm link Customer */}
+            <li className="menu">
+              <Link to="/admin/customer">Customer</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="float-right">
+          Hello <b>{this.context.username}</b> |{" "}
+          <Link to="/admin/home" onClick={() => this.lnkLogoutClick()}>
+            Logout
+          </Link>
+        </div>
+
+        <div className="float-clear" />
+      </div>
+    );
+  }
+
+  // event handlers
+  lnkLogoutClick() {
+    this.context.setToken("");
+    this.context.setUsername("");
+  }
+}
+
+export default Menu;
